3b29efd576da3eb7c9dce6ae5a39f69745c87194
KevinMa0216 <159548454+KevinMa0216@users.noreply.github.com>
2024-02-20 14:23:44 -0500

    Add files via upload

---
+++ promedio.py
@@ -0,0 +1,10 @@
+nota1=3.6
+nota2=2.9
+nota3=3.33
+
+porcentaje1=nota1*0.20
+porcentaje2=nota2*0.35
+porcentaje3=nota3*0.45
+
+promedio=( porcentaje1 + porcentaje2) + porcentaje3
+print ("el promedio del estudiante es", promedio)
---
+++ triangulo.py
@@ -0,0 +1,6 @@
+base=3
+altura=4
+hipotenusa=5
+area=(base*altura)/2
+print ("el area del triangulo es", area)
+
